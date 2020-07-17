c6749cb717946e2817bdcf664411bba0462d7e34
ashhalsyed <ashhalsyed@gmail.com>
2020-07-17 11:37:50 -0400

    main react portfolio site commit

---
+++ build/precache-manifest.8dc4403ee7583e0c9146fa3cf409b646.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f78c2a37a49cecc2d15e3bf88ffc25fb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eaa4f75fd36c4d28ed51",
+    "url": "/static/css/main.0f442888.chunk.css"
+  },
+  {
+    "revision": "ac347447ba863b26c71f",
+    "url": "/static/js/2.d5ed8ffc.chunk.js"
+  },
+  {
+    "revision": "eaa4f75fd36c4d28ed51",
+    "url": "/static/js/main.cc4411fb.chunk.js"
+  },
+  {
+    "revision": "ad9d9003bee0af8dbdc4",
+    "url": "/static/js/runtime-main.5d8ee5e0.js"
+  },
+  {
+    "revision": "553df8f707c6c6e71f2d761be05f006c",
+    "url": "/static/media/Asset 4.553df8f7.svg"
+  },
+  {
+    "revision": "3d5a9329528e79f6e749f5066323c937",
+    "url": "/static/media/PublisherLogo.3d5a9329.svg"
+  },
+  {
+    "revision": "8d5983c24e1fa8c091c4e24d61c6f8df",
+    "url": "/static/media/calcCardsPreview.8d5983c2.png"
+  },
+  {
+    "revision": "28e19d7201e5f15b32539a09dc835bbd",
+    "url": "/static/media/computerScienceAdventureQuizPreview.28e19d72.png"
+  },
+  {
+    "revision": "e232a4f01b4df27f93c166b4c3a40dc6",
+    "url": "/static/media/easyGuiLogo.e232a4f0.png"
+  },
+  {
+    "revision": "bb5d30fec65686babae2e8caa53666c3",
+    "url": "/static/media/firebaseLogo.bb5d30fe.png"
+  },
+  {
+    "revision": "8d5d8901b948c2c67480462ab4938712",
+    "url": "/static/media/fontAwesomeLogo.8d5d8901.svg"
+  },
+  {
+    "revision": "93ceeb6a04bd609e7ff6ea2a3a5d2e73",
+    "url": "/static/media/illustratorLogo.93ceeb6a.svg"
+  },
+  {
+    "revision": "77e0369958cca3672917d720381b7dfc",
+    "url": "/static/media/logoBlack.77e03699.svg"
+  },
+  {
+    "revision": "2e3a096601688ddc3da05f990c949b00",
+    "url": "/static/media/logoWhite.2e3a0966.svg"
+  },
+  {
+    "revision": "23255b5dae05c0bbd8aa689b2c1b3ccd",
+    "url": "/static/media/paintLogo.23255b5d.png"
+  },
+  {
+    "revision": "e38895a159ff5bbd8da2f1040b61d219",
+    "url": "/static/media/pygameLogo.e38895a1.gif"
+  },
+  {
+    "revision": "fbef321e03c16bcae42560f8507757d6",
+    "url": "/static/media/pythonLogo.fbef321e.svg"
+  },
+  {
+    "revision": "62525faad34d6cd8049c32f9cd11ccf3",
+    "url": "/static/media/reactLogo.62525faa.svg"
+  },
+  {
+    "revision": "411caca7bf6a69553ed44410d7893a03",
+    "url": "/static/media/reactRouterLogo.411caca7.png"
+  },
+  {
+    "revision": "22579bc919df0a378c68ec03a3d04136",
+    "url": "/static/media/sassLogo.22579bc9.svg"
+  },
+  {
+    "revision": "c2817d2c8431f7461d616783217f8521",
+    "url": "/static/media/str60Preview.c2817d2c.png"
+  },
+  {
+    "revision": "a49806f9ad28d906c43461f0bee9903b",
+    "url": "/static/media/valorantGuidesPreview.a49806f9.png"
+  },
+  {
+    "revision": "3c5c72a90a7a0c5626c601527cfb205b",
+    "url": "/static/media/valorantLogo.3c5c72a9.svg"
+  }
+]);
