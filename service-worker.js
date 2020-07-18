91290e1f3776056e28fe2592889161d4af9aa563
ashhalsyed <ashhalsyed@gmail.com>
2020-07-18 19:49:10 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.42ca205551c9d2de35d492d1a4558fb6.js"
+  "/precache-manifest.3347ce4d9f3d2d4fb71688b5ab6d0c80.js"
 );
 
 self.addEventListener('message', (event) => {
